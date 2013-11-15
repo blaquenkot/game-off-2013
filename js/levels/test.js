6bd21b469e8e61a60218573a18a6b2dcbf88c4e0
Julio Olivera <julio.olvr@gmail.com>
2013-11-14 21:22:03 -0300

    First level :heart:

--- js/levels/test.js
+++ js/levels/test.js
@@ -17,12 +17,6 @@ define(['../environment', 'water', 'entities/log', 'entities/tools/waterTool'],
 
 				this.water = new Water(this);
 				me.game.world.addChild(this.water);
-
-				// TODO: Added just as a test
-				// this.log = new Log(120, 350);
-				// me.game.world.addChild(this.log);
-				// waterTool = new WaterTool(360, 350);
-				// me.game.world.addChild(waterTool);
 			},
 			waterHeight: function() {
 				return this.environment.waterLevel - this.baseHeight;
