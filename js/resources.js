bb944efeee78ed756e2cfe7cba8af4dd53d8f1ca
Kotzi <kotzi91@gmail.com>
2013-11-26 21:56:39 -0300

    Several minor fixes and first poison level

--- js/resources.js
+++ js/resources.js
@@ -3,6 +3,7 @@ define([], function() {
 		// Graphics
 		{name: 'tile', type: 'image', src: '/game-off-2013/assets/tile.png'},
 		{name: 'pjTile', type: 'image', src: '/game-off-2013/assets/pjtile.png'},
+		{name: 'pantherTile', type: 'image', src: '/game-off-2013/assets/panthertile.png'},
 		{name: 'log', type: 'image', src: '/game-off-2013/assets/log.png'},
 		{name: 'waterTool', type: 'image', src: '/game-off-2013/assets/watertool.png'},
 		{name: 'meltTool', type: 'image', src: '/game-off-2013/assets/melttool.png'},
@@ -23,6 +24,7 @@ define([], function() {
     {name: 'level4', type: 'tmx', src: '/game-off-2013/data/level4.tmx'},
     {name: 'level5', type: 'tmx', src: '/game-off-2013/data/level5.tmx'},
     {name: 'level6', type: 'tmx', src: '/game-off-2013/data/level6.tmx'},
+    {name: 'level7', type: 'tmx', src: '/game-off-2013/data/level7.tmx'},
 
 		// Background Music
 		{name: "background", type: "audio", src: "/game-off-2013/data/music/", channel : 1},
