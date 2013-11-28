0bfc78ed661bcebb38e024d4d9a0f78baff987d7
Julio Olivera <julio.olvr@gmail.com>
2013-11-28 13:39:09 -0300

    Preload font and fix for last level death

--- js/gameEnd.js
+++ js/gameEnd.js
@@ -1,9 +1,11 @@
 define([], function() {
 	var GameEnd = me.CollectableEntity.extend({
 		onDestroyEvent: function() {
-			me.game.viewport.fadeIn('#000', 100, function() {
-				me.state.change(me.state.CREDITS);
-			});
+			if(this.collide()) {
+				me.game.viewport.fadeIn('#000', 100, function() {
+					me.state.change(me.state.CREDITS);
+				});
+			}
 		}
 	});
 
