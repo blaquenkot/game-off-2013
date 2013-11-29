231c353592c683cdf0192cb699c42af055ff14c1
Julio Olivera <julio.olvr@gmail.com>
2013-11-28 22:08:01 -0300

    Nice end state

--- js/states/credits.js
+++ js/states/credits.js
@@ -17,31 +17,51 @@ define(['environment', 'water', 'entities/glacier'], function(Environment, Water
 		return encodeURIComponent('http://joliv.me/game-off-2013');
 	};
 
-	var Credits = me.ScreenObject.extend({
-		init: function() {
+    var DeadOverlay = me.ColorLayer.extend({
+        init: function(state) {
+		  this.parent('deadOverlay', '#C00', Infinity);
+          this.state = state;
+            console.log(me.game.enviromentalStats);
+		  if(me.game.enviromentalStats){
+              this.alpha = (Environment.MAX_YEARS - me.game.enviromentalStats.yearsLeft) / Environment.MAX_YEARS * DeadOverlay.MAX_ALPHA;
+          }
+        },
+    });
+    
+    DeadOverlay.MAX_ALPHA = 0.6;    
+
+	var Credits = me.ScreenObject.extend({    
+        init: function() {
 			this.font = null;
 			this.parent(true);
-			this.twitterCoords = new me.Rect(new me.Vector2d(me.game.world.width / 2 - 55, me.game.world.height - 20), 60, 20);
-			this.facebookCoords = new me.Rect(new me.Vector2d(me.game.world.width / 2 + 10, me.game.world.height - 20), 70, 20);
+			this.twitterCoords = new me.Rect(new me.Vector2d(me.game.world.width / 2 - 50, me.game.world.height - 30), 82, 20); //fix
+			this.facebookCoords = new me.Rect(new me.Vector2d(me.game.world.width / 2 + 45, me.game.world.height - 30), 102, 20); //fix
+            this.background = me.loader.getImage("difusebg");
+            this.overlay = new DeadOverlay(this);
+            me.game.world.addChild(this.overlay);
 		},
 		onResetEvent: function() {
+            this.overlay = new DeadOverlay(this);
+            me.game.world.addChild(this.overlay);
 			var socialMessage = new SocialMessage(me.game.enviromentalStats);
 
 			if (this.font === null) {
-				this.font = new me.Font('wendy', '2em', '#fff');
+				this.font = new me.Font('wendy', '3em', '#fff', 'center');
 			}
 
 			me.game.viewport.fadeOut('#000', 100);
 
 			// Twitter event
 			me.input.registerPointerEvent('mousedown', this.twitterCoords, function() {
-				window.open('https://twitter.com/intent/tweet?hashtags=chchchchchanges&url=' + socialMessage.getUrl() + '&text=' + socialMessage.getEncodedMessage(), 'Tweet!', '_blank,width=420,height=230');
+				console.log("twitter");
+                //window.open('https://twitter.com/intent/tweet?hashtags=chchchchchanges&url=' + socialMessage.getUrl() + '&text=' + socialMessage.getEncodedMessage(), 'Tweet!', '_blank,width=420,height=230');
 			});
 
 			// Facebook event
 			me.input.registerPointerEvent('mousedown', this.facebookCoords, function() {
 				// TODO: Check how to add a specific message
-				window.open('http://www.facebook.com/sharer.php?u=' + socialMessage.getUrl(), 'Share!', '_blank,width=620,height=300');
+				console.log("facebook");
+                //window.open('http://www.facebook.com/sharer.php?u=' + socialMessage.getUrl(), 'Share!', '_blank,width=620,height=300');
 			});
 		},
 		onDestroyEvent: function() {
@@ -52,20 +72,19 @@ define(['environment', 'water', 'entities/glacier'], function(Environment, Water
 			if (me.input.isKeyPressed('reset')) {
 				me.state.change(me.state.MENU);
 			}
-
+           // this.overlay.update();
 			return true;
 		},
 		draw: function(context) {
+            context.drawImage(this.background, 0, 0);
 			var animalsKilled = me.game.enviromentalStats.animalsKilled;
-			context.fillStyle = '#000';
-			context.fillRect(0, 0, me.game.world.width, me.game.world.height);
-			this.font.draw(context, Environment.MAX_YEARS - me.game.enviromentalStats.yearsLeft + ' years lost', me.game.world.width / 2 - 100, me.game.world.height / 2 - 60);
-			this.font.draw(context, Water.toMeters(me.game.enviromentalStats.waterLevel).toFixed(2) + 'm of water raised', me.game.world.width / 2 - 100, me.game.world.height / 2 - 40);
-			this.font.draw(context, Glacier.toCubicCm(me.game.enviromentalStats.iceMelted).toFixed(2) + ' cm3 of ice melted', me.game.world.width / 2 - 100, me.game.world.height / 2 - 20);
-			this.font.draw(context, animalsKilled + (animalsKilled == 1 ? ' animal' : ' animals') + ' killed', me.game.world.width / 2 - 100, me.game.world.height / 2);
-			this.font.draw(context, 'Press 0 to try again', me.game.world.width / 2 - 100, me.game.world.height / 2 + 20);
+			this.font.draw(context, Environment.MAX_YEARS - me.game.enviromentalStats.yearsLeft + ' years lost', me.game.world.width / 2, me.game.world.height / 2 - 50);
+			this.font.draw(context, Water.toMeters(me.game.enviromentalStats.waterLevel).toFixed(2) + 'm of water raised', me.game.world.width / 2, me.game.world.height / 2 - 30);
+			this.font.draw(context, Glacier.toCubicCm(me.game.enviromentalStats.iceMelted).toFixed(2) + ' cm3 of ice melted', me.game.world.width / 2, me.game.world.height / 2 - 10);
+			this.font.draw(context, animalsKilled + (animalsKilled == 1 ? ' animal' : ' animals') + ' killed', me.game.world.width / 2 , me.game.world.height / 2 + 10);
+			this.font.draw(context, 'Press 0 to try again', me.game.world.width / 2, me.game.world.height / 2 + 30);
 
-			this.font.draw(context, 'Share on Twitter | Facebook', me.game.world.width / 2 - 115, me.game.world.height - 20);
+			this.font.draw(context, 'Share on Twitter | Facebook', me.game.world.width / 2, me.game.world.height - 40);
 		}
 	});
 
